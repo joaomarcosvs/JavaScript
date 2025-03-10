03d73fb5a07beea5a4ca614528cecd30da9d80fa
João Marcos <joaomarcos.vieirasantos@gmail.com>
2025-03-10 18:17:47 -0300

    JS Curso
    Apenas comecei, mas preciso rever a ultima aula e continuar amanhã!

---
+++ script.js
@@ -0,0 +1,22 @@
+// const para valor fixos e o let permite alternancia de valor.
+const inputItem = document.getElementById("input-item");
+const botaoAdd = document.getElementById("adicionar-item");
+let contador = 0;
+
+botaoAdd.addEventListener("click", (evento) => {
+    evento.preventDefault();
+    if(inputItem.value == "") {
+        alert("Por favor insira um item!");
+        return;
+    } 
+
+    const itemDaLista = document.createElement("li");
+    const containerItemDaLista = document.createElement("div");
+    containerItemDaLista.classList.add("lista-item-container");
+    const inputCheckbox = document.createElement("input");
+    inputCheckbox.type - "checkbox";
+    inputCheckbox.id = "checkbox-" + contador++;
+
+});
+//funcion call back ou função de retorno que é a mesmsa coisa que uma função dentro de outra
+
