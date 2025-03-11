bbb0a5f97ceabaa25d38faa1e35011bc5ea5dd5a
João Marcos <joaomarcos.vieirasantos@gmail.com>
2025-03-11 15:13:45 -0300

    Criando elementos e os adicionando no HTML
    JavaScript começou a ficar interessante.

--- script.js
+++ script.js
@@ -1,22 +1,42 @@
-// const para valor fixos e o let permite alternancia de valor.
 const inputItem = document.getElementById("input-item");
-const botaoAdd = document.getElementById("adicionar-item");
-let contador = 0;
+const botaoAdicionar = document.getElementById("adicionar-item");
+const listaDeCompras = document.getElementById("lista-de-compras");
+let contador = 0;  // Aqui uso let porque ele diferente da constante pode ser mudado de valor e ele vai servir de i (indice como no C)
 
-botaoAdd.addEventListener("click", (evento) => {
-    evento.preventDefault();
-    if(inputItem.value == "") {
-        alert("Por favor insira um item!");
-        return;
-    } 
+botaoAdicionar.addEventListener("click", (evento) => {
 
+    evento.preventDefault();  //preventDefault previni a ação padrão do que esta sendo executado
+    console.log(inputItem.value);
+
+    if(inputItem.value == ""){
+        alert("Por favor digite algo!");
+        return;        
+    }
+
+    // Será criada uma variavel para inserir um elemento no DOM, no caso vaou criar a lista.
     const itemDaLista = document.createElement("li");
+
     const containerItemDaLista = document.createElement("div");
-    containerItemDaLista.classList.add("lista-item-container");
+    containerItemDaLista.classList.add("lista-item-container"); // Adicionei a classe ao elemento criado.
+    
     const inputCheckbox = document.createElement("input");
-    inputCheckbox.type - "checkbox";
-    //inputCheckbox.id = "checkbox-" + contador++;
+    inputCheckbox.type = "checkbox";
 
-});
-//funcion call back ou função de retorno que é a mesmsa coisa que uma função dentro de outra
+    inputCheckbox.id = "checkbox-" + contador++;  //Aqui simplesmente reproduzi uma repetição para toda vez que o botão for clicado adicionar mais um ao id checkbox-i, para criar varias itens
 
+    const nomeItem = document.createElement("p");
+    nomeItem.innerText = inputItem.value;  // Sempre que o botão for clicado o texto excrito será salvo no P (paragrafo)
+
+    // Tudo precisa ser vinculado ao HTML, então vamos fazer isso com uma arvora, de pai para filho
+    // Está de trás para frente, do ele mais filho ao maior pai
+    
+    containerItemDaLista.appendChild(inputCheckbox);  //INPUT filho do DIV
+    containerItemDaLista.appendChild(nomeItem);  //P filho do DIV
+
+    itemDaLista.appendChild(containerItemDaLista);  //DIV filho do LI
+
+    listaDeCompras.appendChild(itemDaLista); //LI filho do UL
+
+
+}
+)
